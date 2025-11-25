bc5c1f3c5c3a09dcfbc568b37f46bddad02480d1
quan.u <quan.u@seltec.io>
2025-11-25 09:36:30 +0700

    Update dist

---
+++ dist/i18n/index.js
@@ -0,0 +1,8 @@
+export default {
+  "footer.aboutUs": "About Us",
+  "footer.privacyPolicy": "Privacy Policy",
+  "footer.contactUs": "Contact Us",
+  "footer.copyright.text": "© {year} Your Company. All rights reserved.",
+  "footer.edx.copyright": "© 2024 edX Inc. All rights reserved."
+};
+//# sourceMappingURL=index.js.map
